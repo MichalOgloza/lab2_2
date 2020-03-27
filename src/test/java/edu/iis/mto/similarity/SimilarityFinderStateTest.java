e153e957afded950319c6338d6f00891c3d8c55a
Michal <michalogloza97@gmail.com>
2020-03-27 14:58:20 +0100

    Implemented firstSeqEmptyTest, added init method and some constants.

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
@@ -1,15 +1,36 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.MockSearcher;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityFinderStateTest
 {
+    private static final double COMPLETELY_DIFFERENT = 0;
+    private static final double IDENTICAL = 1;
+    private SimilarityFinder similarityFinder;
+    private int [] emptySeq;
+    private int [] oneElementSeq;
+    private int [] exampleSeq;
+    private int [] differentSeq;
+
+    @BeforeEach
+    void init()
+    {
+        similarityFinder = new SimilarityFinder(new MockSearcher());
+        emptySeq = new int [0];
+        oneElementSeq = new int [] {3};
+        exampleSeq = new int [] {9, 3, 7, 1, 0, 6, 200};
+        differentSeq = new int [] {9, 3, 7, 1, 0, 6, 9};
+    }
+
     @Test
     void firstSeqEmptyTest()
     {
-
+        assertEquals(COMPLETELY_DIFFERENT, similarityFinder.calculateJackardSimilarity(emptySeq, exampleSeq));
     }
 
     @Test

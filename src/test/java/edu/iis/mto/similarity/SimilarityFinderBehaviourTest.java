88aaea45f956715afcc359d71c9230f241cf99e7
Michal <michalogloza97@gmail.com>
2020-03-27 14:35:33 +0100

    Implemented firstArgumentNullTest.

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviourTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviourTest.java
@@ -1,16 +1,27 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.MockSearcher;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityFinderBehaviourTest
 {
+    SimilarityFinder similarityFinder;
+    int [] exampleSeq;
+
+    @BeforeEach
+    void init()
+    {
+        similarityFinder = new SimilarityFinder(new MockSearcher());
+        exampleSeq = new int[] {9, 3, 0, 10};
+    }
 
     @Test
     void firstArgumentNullTest()
     {
-
+        assertThrows(NullPointerException.class, () -> similarityFinder.calculateJackardSimilarity(null, exampleSeq));
     }
 
     @Test

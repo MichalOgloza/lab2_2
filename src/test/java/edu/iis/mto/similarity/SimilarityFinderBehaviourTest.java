7b641cbdda1cd7755ea8dfb21508c825b4be24f9
Michal <michalogloza97@gmail.com>
2020-03-27 14:06:46 +0100

    Prepared behaviour tests.

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderBehaviourTest.java
@@ -0,0 +1,33 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimilarityFinderBehaviourTest
+{
+
+    @Test
+    void firstArgumentNullTest()
+    {
+
+    }
+
+    @Test
+    void secondArgumentNullTest()
+    {
+
+    }
+
+    @Test
+    void bothArgumentNullTest()
+    {
+
+    }
+
+    @Test
+    void numberOfCallsTest()
+    {
+
+    }
+}

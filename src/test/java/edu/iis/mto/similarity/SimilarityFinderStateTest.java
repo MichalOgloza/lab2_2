09a96c91ac13a198aa119b3e4a9b928a1a15a3a1
Michal <michalogloza97@gmail.com>
2020-03-27 13:41:45 +0100

    Prepared state tests.

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderStateTest.java
@@ -0,0 +1,63 @@
+package edu.iis.mto.similarity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimilarityFinderStateTest
+{
+    @Test
+    void firstSeqEmptyTest()
+    {
+
+    }
+
+    @Test
+    void secondSeqEmptyTest()
+    {
+
+    }
+
+    @Test
+    void bothSeqEmptyTest()
+    {
+
+    }
+
+    @Test
+    void firstSeqOneElementTest()
+    {
+
+    }
+
+    @Test
+    void secondSeqOneElementTest()
+    {
+
+    }
+
+    @Test
+    void bothSeqOneElementTest()
+    {
+
+    }
+
+    @Test
+    void identicalSeqTest()
+    {
+
+    }
+
+    @Test
+    void completelyDifferentSeqTest()
+    {
+
+    }
+
+    @Test
+    void slightlyDifferentSeqTest()
+    {
+
+    }
+
+}
